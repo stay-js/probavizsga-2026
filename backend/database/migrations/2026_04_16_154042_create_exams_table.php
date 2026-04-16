e41ddf4bd7741e44838ea1b1c1eefee248aaa442
Nagy Zétény <nagyzeteny6@gmail.com>
2026-04-16 17:45:07 +0200

    add migrations, seeders

---
+++ backend/database/migrations/2026_04_16_154042_create_exams_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subject_id')->constrained();
+
+            $table->string('type', 50);
+            $table->string('name', 150);
+            $table->double('score');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exams');
+    }
+};
