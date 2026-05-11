a993249fb98ec2d026e480e23fbde146b5364a03
Nagy Zétény <nagyzeteny6@gmail.com>
2026-05-11 13:45:10 +0200

    index render cards

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 
+import { SubjectCard } from '@/components/card';
 import { getSubjects } from '@/lib/subjects';
 
 export const Route = createFileRoute('/')({
@@ -11,10 +12,14 @@ function RouteComponent() {
   const { data: subjects } = useSuspenseQuery(getSubjects());
 
   return (
-    <div>
-      {subjects?.data.map((subject) => (
-        <div key={subject.id}>{subject.name}</div>
-      ))}
-    </div>
+    <main className="flex flex-col gap-4 py-12">
+      <h1 className="text-center text-3xl font-bold">Tárgyak</h1>
+
+      <div className="grid grid-cols-1 gap-5 p-8 md:grid-cols-2 lg:grid-cols-4">
+        {subjects?.data.map((subject) => (
+          <SubjectCard key={subject.id} subject={subject} />
+        ))}
+      </div>
+    </main>
   );
 }
